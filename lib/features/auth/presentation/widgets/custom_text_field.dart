b6cd004028260ee0309ecd0656359a845bbbe3c4
Ali kamar <aliekamar123@gmail.com>
2025-02-02 22:44:20 +0200

    add custom text field widget

---
+++ lib/features/auth/presentation/widgets/custom_text_field.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:nafas_app/core/utils/app_colors.dart';
+import 'package:nafas_app/core/utils/app_text_styles.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    super.key,
+    required this.lable,
+    required this.hint,
+    required this.icon,
+    this.validator,
+    this.suffixIcon,
+  });
+  final String lable;
+  final String hint;
+  final String icon;
+  final String? suffixIcon;
+  final String? Function(String?)? validator;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 12,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          lable,
+          style: TextStyles.medium12,
+        ),
+        TextFormField(
+          validator: validator,
+          decoration: InputDecoration(
+            hintText: hint,
+            hintStyle: TextStyles.medium12.copyWith(
+              color: AppColors.secondaryTextColor,
+            ),
+            filled: true,
+            fillColor: Colors.white,
+            constraints: BoxConstraints(
+              maxHeight: 55,
+            ),
+            border: border(),
+            enabledBorder: border(),
+            focusedBorder: border(),
+            prefixIcon: SvgPicture.asset(
+              icon,
+              fit: BoxFit.scaleDown,
+            ),
+            suffixIcon: suffixIcon != null
+                ? SvgPicture.asset(
+                    suffixIcon!,
+                    fit: BoxFit.scaleDown,
+                  )
+                : null,
+          ),
+        ),
+      ],
+    );
+  }
+
+  OutlineInputBorder border() {
+    return OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.white,
+      ),
+      borderRadius: BorderRadius.circular(12),
+    );
+  }
+}
