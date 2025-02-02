c79799d980f118e07ed0048ddd436702068ace0c
MohammedEhap007 <mohammedehap007@gmail.com>
2025-02-02 21:45:46 +0200

    add dots indicator

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:nafas_app/features/on_boarding/presentation/views/widgets/page_view_item.dart';
 
 import '../../../../../core/utils/app_images.dart';
@@ -14,7 +13,25 @@ class OnBoardingPageView extends StatelessWidget {
         PageViewItem(
           image: Assets.imagesOnBoardingItem1,
           title: 'عداد توفير يومي',
-          subtitle: 'شاهد كيف تزداد مدخراتك يومًا بعد يوم مع كل سيجارة تتجنبها',
+          subtitle:
+              'شاهد كيف تزداد مدخراتك يومًا بعد يوم مع كل سيجارة تتجنبها.',
+        ),
+        PageViewItem(
+          image: Assets.imagesOnBoardingItem2,
+          title: 'مقالات مفيدة',
+          subtitle:
+              'اقرأ مقالات مُلهمة ونصائح عملية لتدعم قرارك بالإقلاع عن التدخين.',
+        ),
+        PageViewItem(
+          image: Assets.imagesOnBoardingItem3,
+          title: 'فيديوهات تفاعلية مشوقة',
+          subtitle:
+              'اكتشف فيديوهات تعليمية وتحفيزية تساعدك في كل خطوة على الطريق.',
+        ),
+        PageViewItem(
+          image: Assets.imagesOnBoardingItem4,
+          title: 'تجارب ملهمه',
+          subtitle: 'اكتشف قصصًا واقعية لأشخاص تغلبوا على التدخين.',
         ),
       ],
     );
--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:nafas_app/core/utils/app_colors.dart';
 import 'package:nafas_app/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -11,7 +13,19 @@ class OnBoardingViewBody extends StatelessWidget {
       child: Column(
         children: [
           Expanded(child: OnBoardingPageView()),
-          
+          DotsIndicator(
+            dotsCount: 4,
+            decorator: DotsDecorator(
+              color: Color(0XFFD9D9D9),
+              size: const Size(10.0, 10.0),
+              activeColor: AppColors.primaryColor,
+              activeSize: const Size(17.0, 8.0),
+              activeShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+          ),
+          SizedBox(height: 15.0),
         ],
       ),
     );
