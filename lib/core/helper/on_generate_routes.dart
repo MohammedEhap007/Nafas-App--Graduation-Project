fdc646cb35387272aa25d504cdaa888779535842
Ali kamar <aliekamar123@gmail.com>
2025-02-02 22:46:20 +0200

    add routes for login, signup, and forget password views

--- lib/core/helper/on_generate_routes.dart
+++ lib/core/helper/on_generate_routes.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nafas_app/features/auth/presentation/forget_password_view.dart';
+import 'package:nafas_app/features/auth/presentation/log_in_view.dart';
+import 'package:nafas_app/features/auth/presentation/sign_up_view.dart';
 import 'package:nafas_app/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:nafas_app/features/splash/presentation/views/splash_view.dart';
 
@@ -8,6 +11,13 @@ Route onGenerateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => const SplashView());
     case OnBoardingView.routeName:
       return MaterialPageRoute(builder: (context) => const OnBoardingView());
+    case LogInView.routeName:
+      return MaterialPageRoute(builder: (context) => const LogInView());
+    case SignUpView.routeName:
+      return MaterialPageRoute(builder: (context) => const SignUpView());
+    case ForgetPasswordView.routeName:
+      return MaterialPageRoute(
+          builder: (context) => const ForgetPasswordView());
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
   }
