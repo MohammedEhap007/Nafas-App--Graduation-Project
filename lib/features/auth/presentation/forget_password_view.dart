cdc855cd0568aa6b22353cb00e35b57919b7a9e3
Ali kamar <aliekamar123@gmail.com>
2025-02-02 22:46:28 +0200

    add forget password view

---
+++ lib/features/auth/presentation/forget_password_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:nafas_app/core/utils/app_colors.dart';
+import 'package:nafas_app/features/auth/presentation/widgets/forget_password_view_body.dart';
+
+class ForgetPasswordView extends StatelessWidget {
+  const ForgetPasswordView({super.key});
+  static const routeName = 'forgetPassword';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.primaryBackgroundColor,
+      ),
+      body: ForgetPasswordViewBody(),
+    );
+  }
+}
