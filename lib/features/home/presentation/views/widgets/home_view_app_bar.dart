e388fb0b959c9752fdcff04c7df330ad6fe9548d
Ali kamar <aliekamar123@gmail.com>
2025-02-07 02:06:11 +0200

    add HomeViewAppBar widget

---
+++ lib/features/home/presentation/views/widgets/home_view_app_bar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:nafas_app/core/utils/app_custom_icons.dart';
+import 'package:nafas_app/core/utils/app_text_styles.dart';
+
+class HomeViewAppBar extends StatelessWidget {
+  const HomeViewAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SvgPicture.asset(AppCustomIcons.blueHumanIcon),
+        const SizedBox(width: 15),
+        Text(
+          'مرحبا بك , يا محمد أحمد',
+          style: TextStyles.medium16(context),
+        ),
+        Expanded(child: SizedBox()),
+        Container(
+          width: 42,
+          height: 42,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: SvgPicture.asset(AppCustomIcons.bellIcon),
+          ),
+        ),
+      ],
+    );
+  }
+}
