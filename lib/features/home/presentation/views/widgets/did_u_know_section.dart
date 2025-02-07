d8d63c9bc301a8eba1aefc4347ef3172b5c8643f
Ali kamar <aliekamar123@gmail.com>
2025-02-07 02:06:26 +0200

    add DidUKnowSection widget

---
+++ lib/features/home/presentation/views/widgets/did_u_know_section.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:nafas_app/core/utils/app_images.dart';
+import 'package:nafas_app/core/utils/app_text_styles.dart';
+import 'package:nafas_app/core/widgets/custom_container.dart';
+import 'package:nafas_app/features/home/presentation/views/widgets/custom_lable_shape.dart';
+
+class DidUKnowSection extends StatelessWidget {
+  const DidUKnowSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomLableShape(
+          lable: 'هل تعلم ؟',
+        ),
+        SizedBox(
+          height: 12,
+        ),
+        CustomContainer(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 23,
+              vertical: 30,
+            ),
+            child: Row(
+              children: [
+                SvgPicture.asset(Assets.ideaAvatar),
+                SizedBox(width: 15),
+                Expanded(
+                  child: Text(
+                    'الإقلاع عن التدخين لمدة 20 دقيقة فقط يخفض معدل ضربات القلب وضغط الدم',
+                    style: TextStyles.medium16(context),
+                    softWrap: true,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
