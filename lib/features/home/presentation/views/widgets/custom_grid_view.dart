09af5a18e6da456698d976212f4d7f2fee40ef24
Ali kamar <aliekamar123@gmail.com>
2025-02-07 02:05:42 +0200

    add CustomCounterGridView widget

---
+++ lib/features/home/presentation/views/widgets/custom_grid_view.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:nafas_app/core/utils/app_colors.dart';
+import 'package:nafas_app/core/utils/app_images.dart';
+import 'package:nafas_app/core/utils/app_text_styles.dart';
+import 'package:nafas_app/core/widgets/custom_container.dart';
+import 'package:nafas_app/features/home/data/model/counter_model.dart';
+
+class CustomCounterGridView extends StatelessWidget {
+  const CustomCounterGridView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    List<CounterModel> counterData = [
+      CounterModel(
+        image: Assets.boxMoneyAvatar,
+        count: '500 جنيه',
+        title: 'المبلغ المُوفَّر',
+      ),
+      CounterModel(
+        image: Assets.cigaretteAvatar,
+        count: '50 سيجارة',
+        title: 'السجائر المُوفّرة',
+      ),
+      CounterModel(
+        image: Assets.goalAvatar,
+        count: '1000 جنيه',
+        title: 'الهدف المحدد',
+      ),
+      CounterModel(
+        image: Assets.insepctionAvatar,
+        count: '10 هدف',
+        title: 'الأهداف المكتمله',
+      ),
+    ];
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: counterData.length,
+      itemBuilder: (context, index) {
+        return CustomContainer(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 20,
+              // bottom: 20,
+              right: 18.5,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SvgPicture.asset(
+                  counterData[index].image,
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  counterData[index].title,
+                  style: TextStyles.medium16(context),
+                ),
+                Text(
+                  counterData[index].count,
+                  style: TextStyles.medium16(context)
+                      .copyWith(color: AppColors.primaryColor),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
+  }
+}
