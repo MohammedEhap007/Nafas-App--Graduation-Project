16019ebbeb5acd57f12a507d0d0e4f059dabeaa6
MohammedEhap007 <mohammedehap007@gmail.com>
2025-02-14 00:15:33 +0200

    fixing custom dialog header spacing

--- lib/features/savings_counter/presentation/views/widgets/custom_dialog_header.dart
+++ lib/features/savings_counter/presentation/views/widgets/custom_dialog_header.dart
@@ -4,7 +4,7 @@ import 'package:nafas_app/core/utils/app_text_styles.dart';
 
 class CustomDialogHeader extends StatelessWidget {
   const CustomDialogHeader({
-    super.key, 
+    super.key,
     required this.title,
   });
 
@@ -14,28 +14,32 @@ class CustomDialogHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 16.0),
-      child: Row(
-        children: [
-          IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            padding: EdgeInsets.zero,
-            icon: const Icon(
-              Icons.close,
-              color: AppColors.secondaryTextColor,
+      child: Expanded(
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.close,
+                color: AppColors.secondaryTextColor,
+              ),
             ),
-          ),
-          SizedBox(
-            width: 55,
-          ),
-          Text(
-            title,
-            style: TextStyles.medium18(context).copyWith(
-              color: AppColors.primaryColor,
+            Spacer(
+              flex: 1,
             ),
-          ),
-        ],
+            Text(
+              title,
+              style: TextStyles.medium18(context).copyWith(
+                color: AppColors.primaryColor,
+              ),
+            ),
+            Spacer(
+              flex: 2,
+            ),
+          ],
+        ),
       ),
     );
   }
