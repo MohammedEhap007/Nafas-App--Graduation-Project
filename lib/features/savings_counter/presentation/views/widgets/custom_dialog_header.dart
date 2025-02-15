53bc66a30c09aa1824a0a835772262694bf3c1ec
MohammedEhap007 <mohammedehap007@gmail.com>
2025-02-15 02:52:32 +0200

    simple refactor on custom dialog header and splash duration & update gradle file

--- lib/features/savings_counter/presentation/views/widgets/custom_dialog_header.dart
+++ lib/features/savings_counter/presentation/views/widgets/custom_dialog_header.dart
@@ -14,32 +14,30 @@ class CustomDialogHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 16.0),
-      child: Expanded(
-        child: Row(
-          children: [
-            IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: const Icon(
-                Icons.close,
-                color: AppColors.secondaryTextColor,
-              ),
+      child: Row(
+        children: [
+          IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.close,
+              color: AppColors.secondaryTextColor,
             ),
-            Spacer(
-              flex: 1,
+          ),
+          Spacer(
+            flex: 1,
+          ),
+          Text(
+            title,
+            style: TextStyles.medium18(context).copyWith(
+              color: AppColors.primaryColor,
             ),
-            Text(
-              title,
-              style: TextStyles.medium18(context).copyWith(
-                color: AppColors.primaryColor,
-              ),
-            ),
-            Spacer(
-              flex: 2,
-            ),
-          ],
-        ),
+          ),
+          Spacer(
+            flex: 2,
+          ),
+        ],
       ),
     );
   }
