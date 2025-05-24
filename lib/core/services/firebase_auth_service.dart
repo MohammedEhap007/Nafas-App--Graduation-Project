272cb27bd606f8ceed23ddb5489e8ef7b09bab78
MohammedEhap007 <mohammedehap007@gmail.com>
2025-05-24 21:20:17 +0300

    add sign in with google

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:nafas_app/core/errors/exceptions.dart';
 
 class FirebaseAuthService {
@@ -77,6 +78,20 @@ class FirebaseAuthService {
       throw CustomException(
           message: 'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.');
     }
+  }
+
+  Future<User> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    return (await FirebaseAuth.instance.signInWithCredential(credential)).user!;
   }
 
   bool isLoggedIn() => FirebaseAuth.instance.currentUser != null;
